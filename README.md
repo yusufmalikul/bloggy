91af2b9d0b2e498a3189a1021df5b5f718e028e9
yusufmalikul <yusuflinuxer@gmail.com>
2022-09-11 13:50:30 +0700

    add flag and instruction

--- README.md
+++ README.md
@@ -8,10 +8,38 @@ Currently, bloggy is in a very early stage of development. It is not ready for p
 I wanted to create a simple blog generator that is easy to use. I prefer static sites because it's cheap to host and maintain. It also reduce security risks for your server.
 
 # Usage
-Run this command to generate markdown file to your blog directory.
+First, you need to prepare your blog posts content.
+```
+❯ tree myblog
+myblog
+├── layouts
+│   ├── index.html
+│   └── post.html
+└── posts
+    ├── hello-world.md
+    └── second-post.md
+```
+
+Run this command to generate markdown file to your designated output directory.
 
 ```
-bloggy
+bloggy --content myblog/posts --layouts myblog/layouts --output myblog/html
+```
+
+It will generate html files in `myblog/html` directory.
+```
+❯ tree myblog
+myblog
+├── html
+│   ├── hello-world.html
+│   ├── index.html
+│   └── second-post.html
+├── layouts
+│   ├── index.html
+│   └── post.html
+└── posts
+    ├── hello-world.md
+    └── second-post.md
 ```
 
 After that, you can start bloggy as a server.
@@ -19,7 +47,7 @@ After that, you can start bloggy as a server.
 bloggy server
 ```
 
-Visit http://localhost:8080 to see your blog. You can also deploy the static files to your server.
+Visit http://localhost:8080 to see your blog. You can also deploy the static files (`myblog/html`) to your server.
 
 # License
 MIT
--- cmd/root.go
+++ cmd/root.go
@@ -13,12 +13,24 @@ import (
 	"github.com/yuin/goldmark"
 )
 
+func init() {
+	rootCmd.PersistentFlags().String("content", "content", "Content directory")
+	rootCmd.PersistentFlags().String("layouts", "layouts", "Layouts directory")
+	rootCmd.PersistentFlags().String("output", "output", "Output directory")
+}
+
 var rootCmd = &cobra.Command{
 	Use:   "bloggy",
 	Short: "Bloggy is a simple and stup*d blog generator",
 	Long:  `A simple and stup*d blog generator written in Go.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		generate()
+
+		// get flags
+		content, _ := cmd.Flags().GetString("content")
+		layouts, _ := cmd.Flags().GetString("layouts")
+		output, _ := cmd.Flags().GetString("output")
+
+		generate(content, layouts, output)
 	},
 }
 
@@ -39,15 +51,13 @@ type Posts struct {
 	Posts []Post
 }
 
-func generate() {
+func generate(content, layouts, output string) {
 	fmt.Println("Generating...")
 	t := time.Now()
 	var posts Posts
-	sourceDir := "examples/posts"
-	destinationDir := "examples/html"
 
 	// iterate over the directory
-	files, err := os.ReadDir(sourceDir)
+	files, err := os.ReadDir(content)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -55,7 +65,7 @@ func generate() {
 
 	for _, file := range files {
 		// read the file
-		data, err := os.ReadFile(sourceDir + "/" + file.Name())
+		data, err := os.ReadFile(content + "/" + file.Name())
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -65,13 +75,21 @@ func generate() {
 		var buf bytes.Buffer
 		if err = goldmark.Convert(data, &buf); err != nil {
 			fmt.Println(err)
+			return
+		}
+
+		// create output dir if not exist
+		err = os.MkdirAll(output, 0755)
+		if err != nil {
+			fmt.Println(err)
+			return
 		}
 
 		// write the html
 		// remove .md from the file name
 		fileName := file.Name()
 		fileName = fileName[:len(fileName)-3]
-		err = os.WriteFile(destinationDir+"/"+fileName+".html", buf.Bytes(), 0644)
+		err = os.WriteFile(output+"/"+fileName+".html", buf.Bytes(), 0644)
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -82,7 +100,7 @@ func generate() {
 	}
 
 	// generate the index
-	indexLayout := "examples/layouts/index.html"
+	indexLayout := layouts + "/index.html"
 	indexTemplate, err := os.ReadFile(indexLayout)
 	if err != nil {
 		fmt.Println(err)
@@ -97,7 +115,7 @@ func generate() {
 	}
 
 	// write the template
-	destinationIndex := destinationDir + "/index.html"
+	destinationIndex := output + "/index.html"
 	f, err := os.Create(destinationIndex)
 	if err != nil {
 		fmt.Println(err)
@@ -109,6 +127,38 @@ func generate() {
 	if err != nil {
 		fmt.Println(err)
 		return
+	}
+
+	// generate the post
+	postLayout := layouts + "/post.html"
+	postTemplate, err := os.ReadFile(postLayout)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// compile the template
+	tmpl, err = template.New("post").Parse(string(postTemplate))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// write the template
+	for _, post := range posts.Posts {
+		destinationPost := output + "/" + post.Path
+		f, err := os.Create(destinationPost)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// execute the template
+		err = tmpl.Execute(f, post)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	}
 
 	fmt.Println("Done in", time.Since(t))
